39e8c3ca559d816e8bb1267c18bafe8b252ff275
mateuszkrzeminski <mkrzeminski90@gmail.com>
2018-06-11 19:40:08 +0200

    first

---
+++ RebarPhasesManager/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Tekla.Structures.Model.UI;
+
+namespace RebarPhasesManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PhaseManager phaseManager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            phaseManager = FindResource("phaseManager") as PhaseManager;
+        }
+
+        private void button_Set_Click(object sender, RoutedEventArgs e)
+        {
+            phaseManager.SetTemporaryColor();
+
+        }
+
+        private void button_Reset_Click(object sender, RoutedEventArgs e)
+        {
+            ModelObjectVisualization.ClearAllTemporaryStates();
+
+        }
+
+        public void Checked(object sender, RoutedEventArgs e)
+        {
+              phaseManager.SetTemporaryColor();
+        }
+
+        public void Unchecked(object sender, RoutedEventArgs e)
+        {
+              phaseManager.SetTemporaryColor();
+        }
+
+    }
+}
---
+++ RebarPhasesManager/ModelExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Tekla.Structures.Model;
+
+
+namespace RebarPhasesManager
+{
+    public static class ModelExtensionMethods
+    {
+        public static Phase WhatIsMyPhase (this Reinforcement rf)
+        {
+            Phase phase;
+            if (rf.GetPhase(out phase))
+                return phase;
+            else return null;
+        }
+    }
+}
